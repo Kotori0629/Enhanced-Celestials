4cfa82294b045bfd0921d13589c57bdf42c8d036
Kotori0629 <79943751@qq.com>
2024-12-28 13:53:22 +0800

    fix no refmap loaded

--- Common/src/main/java/corgitaco/enhancedcelestials/mixin/MixinBlock.java
+++ Common/src/main/java/corgitaco/enhancedcelestials/mixin/MixinBlock.java
@@ -2,8 +2,8 @@ package corgitaco.enhancedcelestials.mixin;
 
 import corgitaco.enhancedcelestials.EnhancedCelestialsWorldData;
 import corgitaco.enhancedcelestials.core.EnhancedCelestialsContext;
-import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -12,14 +12,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.function.Supplier;
+
 @Mixin(Block.class)
 public class MixinBlock {
-    @Inject(method = "popResource*", at = @At("HEAD"))
-    private static void modifyDrops(Level world, BlockPos pos, ItemStack stack, CallbackInfo ci) {
-        if (!world.isClientSide) {
-            EnhancedCelestialsContext enhancedCelestialsContext = ((EnhancedCelestialsWorldData) world).getLunarContext();
+    @Inject(method = "popResource(Lnet/minecraft/world/level/Level;Ljava/util/function/Supplier;Lnet/minecraft/world/item/ItemStack;)V", at = @At("HEAD"))
+    private static void modifyDrops(Level level, Supplier<ItemEntity> itemEntitySupplier, ItemStack stack, CallbackInfo ci) {
+        if (!level.isClientSide) {
+            EnhancedCelestialsContext enhancedCelestialsContext = ((EnhancedCelestialsWorldData) level).getLunarContext();
             if (enhancedCelestialsContext != null) {
-                enhancedCelestialsContext.getLunarForecast().currentLunarEvent().value().onBlockItemDrop((ServerLevel) world, stack);
+                enhancedCelestialsContext.getLunarForecast().currentLunarEvent().value().onBlockItemDrop((ServerLevel) level, stack);
             }
         }
     }
